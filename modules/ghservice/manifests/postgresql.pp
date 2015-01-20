5dd863b309f33d23e751d20ab3becffcde83e1f2
Raymond Chan <ucamrc@users.noreply.github.com>
2015-01-20 17:21:26 +0000

    Install postgres, create user and db

--- modules/ghservice/manifests/postgresql.pp
+++ modules/ghservice/manifests/postgresql.pp
@@ -1,1 +1,13 @@
-class ghservice::postgresql {}
+class ghservice::postgresql (
+        $db = 'grasshopper',
+        $user = 'grasshopper',
+        $password = 'grasshopper',
+    ) {
+
+    class { 'postgresql::server': }
+
+    postgresql::server::db { "$db":
+        user     => "$user",
+        password => postgresql_password("$user", "$password"),
+    }
+}
