a49aa9dc04f18b534abd16218188d9e42a7d430e
Raymond Chan <ucamrc@users.noreply.github.com>
2015-01-26 16:12:06 +0000

    Apache config - work in progress

--- modules/ghservice/manifests/apache.pp
+++ modules/ghservice/manifests/apache.pp
@@ -1,6 +1,24 @@
 class ghservice::apache {
 
-    ## class { '::apache': {
-    ##     extra_config => template('grasshopper/httpd.conf.erb')
-    ## }
+    class { '::apache': 
+## REDO THIS FROM UBUNTU VERSION ??
+## or is standard one good enough (with the two mod enables below)
+##        conf_template => 'ghservice/apache/httpd.conf.erb'
+    }
+
+    class { '::apache::mod::proxy': }
+    class { '::apache::mod::rewrite': }
+
+### CHECK IF THIS IS THE RIGHT WAY TO DO THIS?
+
+    file { '/etc/apache2/sites-enabled/app_admin.conf':
+        ensure  => file,
+        content => template('ghservice/apache/app_admin.conf.erb')
+    }
+
+    file { '/etc/apache2/sites-enabled/app_timetable.conf':
+        ensure  => file,
+        content => template('ghservice/apache/app_timetable.conf.erb')
+    }
+
 }
