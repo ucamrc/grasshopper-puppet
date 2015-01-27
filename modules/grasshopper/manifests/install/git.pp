0e1bc7cf24f17f24549a4d29832dc3b4eab8bd68
Raymond Chan <ucamrc@users.noreply.github.com>
2015-01-27 12:39:39 +0000

    Install node packages (no need for hiredis workaround inherited from avocet)

--- modules/grasshopper/manifests/install/git.pp
+++ modules/grasshopper/manifests/install/git.pp
@@ -26,10 +26,7 @@ class grasshopper::install::git ($install_config, $app_root_dir = '/opt/grasshop
     # Install node packages
     exec { "npm_install_dependencies":
         cwd         => $app_root_dir,
-
-        # Forcing CFLAGS for std=c99 for hiredis, until https://github.com/pietern/hiredis-node/pull/33 is resolved
-        environment => ['CFLAGS="-std=c99"', 'HOME=/root'],
-        command     => 'npm install -d',
+        command     => 'npm install --production',
         logoutput   => 'on_failure',
     }
 
