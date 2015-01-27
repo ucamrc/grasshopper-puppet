5dca294c197c7da57c80226968e731cfdeb8e11a
Raymond Chan <ucamrc@users.noreply.github.com>
2015-01-27 17:14:39 +0000

    Make sure npm is installed before trying to run it

--- modules/ghservice/manifests/deps/nodejs.pp
+++ modules/ghservice/manifests/deps/nodejs.pp
@@ -5,6 +5,10 @@ class ghservice::deps::nodejs () {
 
     # Apply the nodejs class which will configure the apt repo and install nodejs
     class { '::nodejs':
+        # "before" line IS VITAL to ensure that things that depend on the parent class will actually wait for ::nodejs to finish first
+        # otherwise e.g. npm install would try to run before npm was installed
+        # not sure if there is a better way to do this
+        before => Class['::ghservice::deps::nodejs'],
         version => $nodejs_version,
         manage_repo => true
     }
