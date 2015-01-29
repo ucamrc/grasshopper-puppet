467ad7f17fa5e85a9f80dbd5d867d7be03ffa76c
Raymond Chan <ucamrc@users.noreply.github.com>
2015-01-29 16:09:06 +0000

    Update nodejs version because previous one has vanished

--- modules/ghservice/manifests/deps/nodejs.pp
+++ modules/ghservice/manifests/deps/nodejs.pp
@@ -1,7 +1,7 @@
 class ghservice::deps::nodejs () {
 
     # Get the version from hiera
-    $nodejs_version = hiera('nodejs_version', '0.10.35-1nodesource1~trusty1')
+    $nodejs_version = hiera('nodejs_version', '0.10.36-1nodesource1~trusty1')
 
     # Apply the nodejs class which will configure the apt repo and install nodejs
     class { '::nodejs':
