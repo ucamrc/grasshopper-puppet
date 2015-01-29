2ceedf42a9540825250c9a4b800cc495c976bd05
Raymond Chan <ucamrc@users.noreply.github.com>
2015-01-29 13:06:21 +0000

    paramterise admin hostname, abort on error

--- provisioning/setup-via-api.sh
+++ provisioning/setup-via-api.sh
@@ -1,28 +1,29 @@
-TENANTHOSTNAME=$1
+#!/usr/bin/env bash
 
-if [[ X"" = X"$TENANTHOSTNAME" ]]; then
-echo "ERROR: Please supply tenant hostname"
+ADMINHOSTNAME=$1
+TENANTHOSTNAME=$2
+
+if [[ X"" = X"$ADMINHOSTNAME" || X"" = X"$TENANTHOSTNAME" ]]; then
+echo "Usage: $0 admin.domain.com tenant.domain.com"
   exit 0;
 fi
 
-##### Setup Tenant using local-only admin.grasshopper.com #####
-
 # LOGIN
-curl -b /tmp/curlcookiejar -c /tmp/curlcookiejar -w '\nHTTP STATUS: %{http_code}\nTIME: %{time_total}\n' -e / admin.grasshopper.com/api/auth/login -X POST -d 'password=administrator&username=administrator'
+curl -b /tmp/curlcookiejar -c /tmp/curlcookiejar -w '\nHTTP STATUS: %{http_code}\nTIME: %{time_total}\n' -e / ${ADMINHOSTNAME}:2000/api/auth/login -X POST -d 'password=administrator&username=administrator' || exit 1;
 
 # TODO: change the password when it is implemented(!)
 
 # LIST TENANTS (CHECK LOGGED IN OK)
-curl -b /tmp/curlcookiejar -c /tmp/curlcookiejar -w '\nHTTP STATUS: %{http_code}\nTIME: %{time_total}\n' -e / admin.grasshopper.com/api/tenants
+curl -b /tmp/curlcookiejar -c /tmp/curlcookiejar -w '\nHTTP STATUS: %{http_code}\nTIME: %{time_total}\n' -e / ${ADMINHOSTNAME}:2000/api/tenants || exit 1;
 
 # CREATE TENANT
-curl -b /tmp/curlcookiejar -c /tmp/curlcookiejar -w '\nHTTP STATUS: %{http_code}\nTIME: %{time_total}\n' -e / admin.grasshopper.com/api/tenants -X POST -d 'displayName=mytenant'
+curl -b /tmp/curlcookiejar -c /tmp/curlcookiejar -w '\nHTTP STATUS: %{http_code}\nTIME: %{time_total}\n' -e / ${ADMINHOSTNAME}:2000/api/tenants -X POST -d 'displayName=mytenant' || exit 1;
 ### assume returns "id":1
 
 # CREATE APP (needs tenantId from above, change host param to match)
-curl -b /tmp/curlcookiejar -c /tmp/curlcookiejar -w '\nHTTP STATUS: %{http_code}\nTIME: %{time_total}\n' -e / admin.grasshopper.com/api/apps -X POST -d 'displayName=myapp&tenantId=1&host='$TENANTHOSTNAME'&type=timetable'
+curl -b /tmp/curlcookiejar -c /tmp/curlcookiejar -w '\nHTTP STATUS: %{http_code}\nTIME: %{time_total}\n' -e / ${ADMINHOSTNAME}:2000/api/apps -X POST -d 'displayName=myapp&tenantId=1&host='$TENANTHOSTNAME'&type=timetable' || exit 1;
 ### assume returns "id":1
 
 # CREATE APP USER (needs appId from above)
-curl -b /tmp/curlcookiejar -c /tmp/curlcookiejar -w '\nHTTP STATUS: %{http_code}\nTIME: %{time_total}\n' -e / admin.grasshopper.com/api/users -X POST -d 'appId=1&displayName=TestUser&email=test&password=test'
+curl -b /tmp/curlcookiejar -c /tmp/curlcookiejar -w '\nHTTP STATUS: %{http_code}\nTIME: %{time_total}\n' -e / ${ADMINHOSTNAME}:2000/api/users -X POST -d 'appId=1&displayName=TestUser&email=test&password=test' || exit 1;
 
