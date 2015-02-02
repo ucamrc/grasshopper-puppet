3720b6cf6115de19dc2faac3607697182d2ed46d
Raymond Chan <ucamrc@users.noreply.github.com>
2015-02-02 17:43:54 +0000

    Cant access port 2001 using outside hostname

--- environments/dev/modules/localconfig/manifests/hostnames.pp
+++ environments/dev/modules/localconfig/manifests/hostnames.pp
@@ -5,6 +5,10 @@ class localconfig::hostnames {
     $app_admin_tenant = hiera('app_admin_tenant', 'admin')
     $admin_domain = "${app_admin_tenant}.${web_domain}"
 
-    # This won't be in DNS so define it locally
+    # These are defined to allow puppet and setup scripts to access
+    # the node server ports on 2000 and 2001 locally, whilst still
+    # using external hostnames, and not opening those ports to the world
+    # Additionally, admin_domain won't be in DNS either
     host { $admin_domain: ip => '127.0.0.1' }
+    host { $web_domain: ip => '127.0.0.1' }
 }
