7cd38a72f85241d5118ba375957687f1c8360fc7
Raymond Chan <ucamrc@users.noreply.github.com>
2015-02-03 17:11:46 +0000

    Remove provisional comment

--- modules/ghservice/manifests/postgresql.pp
+++ modules/ghservice/manifests/postgresql.pp
@@ -13,8 +13,6 @@ class ghservice::postgresql (
         password => postgresql_password("$user", "$password"),
     } ->
     exec { 'enable pg_trgm':
-        # Not sure if postgres module supports this natively?
-        # Perhaps this should be done in the code itself?
         command => "sudo -u postgres psql grasshopper -c 'CREATE EXTENSION IF NOT EXISTS pg_trgm'"
     }
 }
