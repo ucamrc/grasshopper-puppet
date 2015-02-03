d766b67c43ac485f495540f328b040f9ba77bc51
Raymond Chan <ucamrc@users.noreply.github.com>
2015-02-03 16:37:27 +0000

    use anchor to fix containment problem more conventionally

--- modules/ghservice/manifests/deps/nodejs.pp
+++ modules/ghservice/manifests/deps/nodejs.pp
@@ -4,11 +4,19 @@ class ghservice::deps::nodejs (
 
     # Apply the nodejs class which will configure the apt repo and install nodejs
     class { '::nodejs':
-        # "before" line IS VITAL to ensure that things that depend on the parent class will actually wait for ::nodejs to finish first
-        # otherwise e.g. npm install would try to run before npm was installed
-        # not sure if there is a better way to do this
-        before => Class['::ghservice::deps::nodejs'],
         version => $nodejs_version,
         manage_repo => true
     }
+
+    # This next section is necessary to ensure the above class
+    # is actually contained within the surrounding class
+    # because by default puppet will let that nodejs declaration float freely in
+    # the dependency graph, resulting in commands like npm install trying
+    # to run too early.
+    # https://docs.puppetlabs.com/puppet/latest/reference/lang_containment.html#anchor-pattern-containment-for-compatibility-with-puppet--340
+
+    anchor { 'nodejs_begin': } ->
+    Class ['::nodejs'] ->
+    anchor { 'nodejs_end': }
+
 }
