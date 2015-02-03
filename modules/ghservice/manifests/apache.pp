5ad4816859625025b6223f4cc6cacfc26ca39a2e
Raymond Chan <ucamrc@users.noreply.github.com>
2015-02-03 13:37:42 +0000

    Apache config: use apache module custom fragment feature, plus minor tweaks

--- modules/ghservice/manifests/apache.pp
+++ modules/ghservice/manifests/apache.pp
@@ -2,6 +2,11 @@ class ghservice::apache (
     $enable_basic_auth = "false" 
     ) {
 
+    if $enable_basic_auth == 'true' {
+      # This package provides the "htpasswd" command for setting passwords
+      package { 'apache2-utils': }
+    }
+
     class { '::apache': 
         default_vhost => false,
     }
@@ -10,35 +15,62 @@ class ghservice::apache (
     class { '::apache::mod::proxy_http': }
     class { '::apache::mod::rewrite': }
 
-    if str2bool($enable_basic_auth) {
-      # This package provides the "htpasswd" command for setting passwords
-      package { 'apache2-utils': }
-    }
-
-    apache::listen { '80': }
-
-### CHECK IF THIS IS THE RIGHT WAY TO DO THIS?
-
     # FIXME this duplicates code in modules/ghservice/manifests/grasshopper.pp
     $web_domain = hiera('web_domain')
     $app_admin_tenant = hiera('app_admin_tenant', 'admin')
     $admin_domain = "${app_admin_tenant}.${web_domain}"
 
     $admin_servername = $admin_domain
+    $tenant_servername = $web_domain
+    $apache_dir_require = $enable_basic_auth ? {
+        'true'  => 'valid-user',
+        default => 'all granted',
+    }
 
-    # VirtualHosts are loaded in alphabetical order
+    # VirtualHosts are loaded in alphabetical order;
+    #
     # Requests that do not match a specified ServerName directive
     # get sent to the default VirtualHost, which is always the first one.
     # See https://httpd.apache.org/docs/2.4/vhosts/examples.html
+    #
+    # We assume that requests that do not match are new tenants.
+    # (In production we would probably want the default VirtualHost
+    # to show an error message instead).
 
-    file { '/etc/apache2/sites-enabled/000-app_timetable.conf':
-        ensure  => file,
-        content => template('ghservice/apache/app_timetable.conf.erb')
+    apache::vhost { 'app_timetable':
+        priority        => 10,
+        vhost_name      => '*',
+        port            => '80',
+        servername      => $tenant_servername,
+        docroot         => "/opt/grasshopper-ui/apps/timetable/ui",
+        directories     => [
+            { 'path'      => "/opt/grasshopper-ui/apps/timetable/ui",
+              'require'   => $apache_dir_require,
+            }
+        ],
+        # We prefer to control the rest of the file ourselves
+        # so stop the module from generating other stuff
+        error_log       => false,
+        access_log      => false,
+        custom_fragment => template('ghservice/apache/app_timetable.conf.erb'),
     }
 
-    file { '/etc/apache2/sites-enabled/100-app_admin.conf':
-        ensure  => file,
-        content => template('ghservice/apache/app_admin.conf.erb')
+    apache::vhost { 'app_admin':
+        priority        => 99,
+        vhost_name      => '*',
+        port            => '80',
+        servername      => $admin_servername,
+        docroot         => "/opt/grasshopper-ui/apps/admin/ui",
+        directories     => [
+            { 'path'      => "/opt/grasshopper-ui/apps/admin/ui",
+              'require'   => $apache_dir_require,
+            }
+        ],
+        # We prefer to control the rest of the file ourselves
+        # so stop the module from generating other stuff
+        error_log       => false,
+        access_log      => false,
+        custom_fragment => template('ghservice/apache/app_admin.conf.erb'),
     }
 
 }
