7f16062d3d4b52df6f5039e73f5663b48ad17fa4
Raymond Chan <ucamrc@users.noreply.github.com>
2015-02-05 15:33:38 +0000

    QA env: start afresh with a copy of Dev env

--- environments/qa/modules/localconfig/manifests/nodes.pp
+++ environments/qa/modules/localconfig/manifests/nodes.pp
@@ -1,10 +1,5 @@
-node 'qa0' {
-    $nodetype = 'qa'
+node 'dev0' {
+    $nodetype = 'dev'
     $nodesuffix = 0
-    hiera_include(classes)
-}
-
-node 'puppet' {
-    $nodetype = 'puppet'
     hiera_include(classes)
 }
