166ba93f420ee20523de9bf7fc2fbed283a3e421
Raymond Chan <ucamrc@users.noreply.github.com>
2015-02-27 13:18:50 +0000

    parameterise shibboleth2.xml template

--- modules/ghservice/manifests/shibboleth.pp
+++ modules/ghservice/manifests/shibboleth.pp
@@ -1,5 +1,7 @@
 class ghservice::shibboleth {
 
+  $shibsp_hostname = hiera('shibsp_hostname')
+
   file { '/etc/shibboleth/shibboleth2.xml':
     content => template('ghservice/shibboleth/shibboleth2.xml.erb'),
     # TODO: notify => Service['shibd'] ? (need to create one of those first?)
