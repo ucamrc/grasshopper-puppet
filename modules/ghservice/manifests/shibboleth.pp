bab3f3e1c63e9a9c5442e8201d9e9e3a027ef38d
Raymond Chan <ucamrc@users.noreply.github.com>
2015-03-04 13:58:11 +0000

    Automatically generate credentials used by CredentialResolver to talk to IdP

--- modules/ghservice/manifests/shibboleth.pp
+++ modules/ghservice/manifests/shibboleth.pp
@@ -2,13 +2,11 @@ class ghservice::shibboleth {
 
   $shibsp_hostname = hiera('shibsp_hostname')
 
-
   # Reduce this in production!
   $clock_skew = 1800
 
   ## POSSIBLY OVERRIDE THIS??
   $sp_entityid = "https://${shibsp_hostname}/shibboleth"
-
 
   # May want to review this in production
   $session_handler_show_attribute_values = "true"
@@ -17,6 +15,14 @@ class ghservice::shibboleth {
   $idp_entityid = "https://idp.testshib.org/idp/shibboleth"
   $idp_metadata_uri = "http://www.testshib.org/metadata/testshib-providers.xml"
   $idp_metadata_localfile = "testshib-two-idp-metadata.xml"
+
+  # IN PRODUCTION WILL PROBABLY WANT TO DO THIS MANUALLY??
+  # This will affect the SP Metadata to be registered with IdP!
+  exec { "shib-keygen -h ${shibsp_hostname}":
+    # This also creates /etc/shibboleth/sp-cert.pem
+    creates => "/etc/shibboleth/sp-key.pem",
+    notify  => Service['shibd'],
+  } ->
 
   file { '/etc/shibboleth/shibboleth2.xml':
     content => template('ghservice/shibboleth/shibboleth2.xml.erb'),
