373133dee9cc22186e00f651eacada981d63c431
Raymond Chan <ucamrc@users.noreply.github.com>
2015-03-04 16:19:07 +0000

    Raven settings

--- modules/ghservice/manifests/shibboleth.pp
+++ modules/ghservice/manifests/shibboleth.pp
@@ -11,10 +11,25 @@ class ghservice::shibboleth {
   # May want to review this in production
   $session_handler_show_attribute_values = "true"
 
+  ################################################
   # IdP settings
-  $idp_entityid = "https://idp.testshib.org/idp/shibboleth"
-  $idp_metadata_uri = "http://www.testshib.org/metadata/testshib-providers.xml"
-  $idp_metadata_localfile = "testshib-two-idp-metadata.xml"
+  ################################################
+
+  # ARGH $idp_entityid MUST MATCH THE TENANT/APP CONFIG IN THE DATABASE TOO!!
+  # TODO: download and review http://<shib-sp.hostname>/Shibboleth.sso/Metadata
+  # Then register that SP Metadata with your IdP (e.g. Raven or TestShib)
+
+  # Raven/Shibboleth settings
+  $idp_entityid = "https://shib.raven.cam.ac.uk/shibboleth"
+  $idp_metadata_uri = "https://shib.raven.cam.ac.uk/ucamfederation-idp2-metadata.xml"
+  $idp_metadata_localfile = "ucamfederation-idp2-metadata.xml"
+  # TestShib settings
+  # $idp_entityid = "https://idp.testshib.org/idp/shibboleth"
+  # $idp_metadata_uri = "http://www.testshib.org/metadata/testshib-providers.xml"
+  # $idp_metadata_localfile = "testshib-two-idp-metadata.xml"
+
+  #################################################
+
 
   # IN PRODUCTION WILL PROBABLY WANT TO DO THIS MANUALLY??
   # This will affect the SP Metadata to be registered with IdP!
