d0bb5ecc8ff0e005802c7e73eb34951d9fcd7330
Raymond Chan <ucamrc@users.noreply.github.com>
2015-03-04 12:56:54 +0000

    Shib config - parameterise, change entityID back to https, merge with Shib/Raven example

--- modules/ghservice/manifests/shibboleth.pp
+++ modules/ghservice/manifests/shibboleth.pp
@@ -2,6 +2,22 @@ class ghservice::shibboleth {
 
   $shibsp_hostname = hiera('shibsp_hostname')
 
+
+  # Reduce this in production!
+  $clock_skew = 1800
+
+  ## POSSIBLY OVERRIDE THIS??
+  $sp_entityid = "https://${shibsp_hostname}/shibboleth"
+
+
+  # May want to review this in production
+  $session_handler_show_attribute_values = "true"
+
+  # IdP settings
+  $idp_entityid = "https://idp.testshib.org/idp/shibboleth"
+  $idp_metadata_uri = "http://www.testshib.org/metadata/testshib-providers.xml"
+  $idp_metadata_localfile = "testshib-two-idp-metadata.xml"
+
   file { '/etc/shibboleth/shibboleth2.xml':
     content => template('ghservice/shibboleth/shibboleth2.xml.erb'),
   } ~>
